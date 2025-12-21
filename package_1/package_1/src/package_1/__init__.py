b65ef89d5ea8b69a12dc021701cc2a1b589cb42c
Guillaume Lemaitre <guillaume@probabl.ai>
2025-12-21 12:36:34 +0100

    initial commit

---
+++ package_1/package_1/src/package_1/__init__.py
@@ -0,0 +1,4 @@
+import numpy  # this is a dependency
+
+__version__ = "0.1.0"
+
---
+++ package_2/package_2/src/package_2/__init__.py
@@ -0,0 +1,5 @@
+import numpy  # this is a dependency
+import package_1  # my own dependency
+
+__version__ = "0.1.0"
+
